6a744cc664c2f4a1a0182cd05c10607c33b5185a
mwaurallan <allanmwaura2013@gmail.com>
2021-03-31 19:30:59 +0300

    first commit

---
+++ application/views/backend/footer.php
@@ -0,0 +1,6 @@
+<!-- Footer -->
+<footer class="main">
+
+	&copy; 2016 <strong><?php echo $system_name; ?> | Version 1.4</strong> Developed by <a href="http://creativeitem.com" target="_blank">Creativeitem</a>
+
+</footer>
